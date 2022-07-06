cfd22ef8a730ef062c56db5e6d00de3afbf3f51b
Ram K Rijal <rijal.ramk@gmail.com>
2022-07-06 11:33:57 +0200

    Importing Order Fixed.

--- src/paper/task_paper.py
+++ src/paper/task_paper.py
@@ -10,27 +10,6 @@ from src.config import SRC
 
 documents = ["research_paper", "research_pres_30min"]
 
-
-# @pytask.mark.latex(
-#     [
-#         "--pdf",
-#         "--interaction=nonstopmode",
-#         "--synctex=1",
-#         "--cd",
-#         "--quiet",
-#         "--shell-escape",
-#     ]
-# )
-# @pytask.mark.parametrize(
-#     "depends_on, produces",
-#     [
-#         (SRC / "paper" / f"{document}.tex", BLD / "paper" / f"{document}.pdf")
-#         for document in documents
-#     ],
-# )
-# def task_compile_documents():
-#     pass
-
 for document in documents:
 
     @pytask.mark.task
@@ -61,17 +40,3 @@ for document in documents:
 )
 def task_copy_to_root(depends_on, produces):
     shutil.copy(depends_on, produces)
-
-
-# for document in documents:
-
-#     @pytask.mark.task
-#     @pytask.mark.latex(
-#         script=BLD / "paper" / f"{document}.pdf",
-#         document= ROOT / f"{document}.pdf",
-#         # compilation_steps=cs.latexmk(
-#         #     (f"--{document}", "--interaction=nonstopmode", "--pdf", "--quiet", "--shell-escape","--synctex=1", "--cd")
-#         # ),
-#     )
-#     def task_copy_to_root(depends_on, produces):
-#         shutil.copy(depends_on, produces)
